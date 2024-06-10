a9993085a131ac8782cff1d38c087eb896d48e74
Marcin Benke <marcin@benke.org>
2024-06-10 12:02:27 +0200

    Language.Core

---
+++ src/Language/Core.hs
@@ -0,0 +1,93 @@
+module Language.Core where
+import Common.Pretty
+import Language.Yul
+
+-- type Name = String
+
+data Type
+    = TInt
+    | TBool
+    | TPair Type Type
+    | TSum Type Type
+    | TFun [Type] Type
+    | TUnit
+    deriving (Show)
+
+data Expr
+    = EInt Int
+    | EBool Bool
+    | EVar Name
+    | EPair Expr Expr
+    | EFst Expr
+    | ESnd Expr
+    | EInl Expr
+    | EInr Expr
+    | ECall Name [Expr]
+    | EUnit
+instance Show Expr where
+    show = render . pretty
+
+pattern SAV :: Name -> Expr -> Stmt
+pattern SAV x e = SAssign (EVar x) e
+data Stmt
+    = SAssign Expr Expr
+    | SAlloc Name Type
+    | SExpr Expr
+    | SAssembly [YulStatement]
+    | SReturn Expr
+    | SComment String
+    | SBlock [Stmt]
+    | SCase Expr [Alt]
+    | SFunction Name [Arg] Type [Stmt]
+    -- deriving Show
+
+data Arg = TArg Name Type
+instance Show Stmt where show = render . pretty
+
+data Alt = Alt Name Stmt
+
+data Core = Core [Stmt]
+
+instance Pretty Type where
+    pretty TInt = text "int"
+    pretty TBool = text "bool"
+    pretty TUnit = text "unit"
+    pretty (TPair t1 t2) = parens (pretty t1 <+> text "*" <+> pretty t2)
+    pretty (TSum t1 t2) = parens (pretty t1 <+> text "+" <+> pretty t2)
+    pretty (TFun ts t) = parens (hsep (map pretty ts) <+> text "->" <+> pretty t)
+
+instance Pretty Expr where
+    pretty (EInt i) = text (show i)
+    pretty (EBool b) = text (show b)
+    pretty EUnit = text "()"
+    pretty (EVar x) = text x
+    pretty (EPair e1 e2) = parens (pretty e1 >< comma <+> pretty e2)
+    pretty (EFst e) = pretty e  >< text ".fst"
+    pretty (ESnd e) = pretty e  >< text ".snd"
+    pretty (EInl e) = text "inl" >< parens (pretty e)
+    pretty (EInr e) = text "inr" >< parens (pretty e)
+    pretty (ECall f es) = text f >< parens(hsep (punctuate comma (map pretty es)))
+
+instance Pretty Stmt where
+    pretty (SAssign lhs rhs) = pretty lhs <+> text ":=" <+> pretty rhs
+    pretty (SAlloc x t) = text "let" <+> text x <+> text ":" <+> pretty t
+    pretty (SExpr e) = pretty e
+    pretty (SAssembly stmts) = vcat (map pretty stmts)
+    pretty (SReturn e) = text "return" <+> pretty e
+    pretty (SComment c) = text "//" <+> text c
+    pretty (SBlock stmts) = lbrace $$ nest 4 (vcat (map pretty stmts)) $$ rbrace
+    pretty (SCase e alts) =
+        text "match" <+> pretty e <+> text "with" $$ lbrace
+           $$ nest 2 (vcat (map pretty alts))  $$ rbrace
+    pretty (SFunction f args ret stmts) =
+        text "function" <+> text f <+> parens (hsep (punctuate comma (map pretty args))) <+> text "->" <+> pretty ret <+> lbrace
+           $$ nest 2 (vcat (map pretty stmts))  $$ rbrace
+
+instance Pretty Arg where
+    pretty (TArg n t) = text n <+> text ":" <+> pretty t
+
+instance Pretty Alt where
+    pretty (Alt n s) = text n <+> text "=>" <+> pretty s
+
+instance Pretty Core where
+    pretty (Core stmts) = vcat (map pretty stmts)
---
+++ src/Language/Core/Parser.hs
@@ -0,0 +1,110 @@
+module Language.Core.Parser where
+import Language.Core
+import Common.LightYear
+import Text.Megaparsec.Char.Lexer qualified as L
+import Control.Monad.Combinators.Expr
+import Language.Yul.Parser(parseYul)
+
+parseCore :: String -> Core
+parseCore = runMyParser coreProgram
+
+-- Note: this module repeats some definitions from YulParser.Name
+-- This is intentional as we may want to make different syntax choices
+
+sc :: Parser ()
+sc = L.space space1
+             (L.skipLineComment "//")
+             (L.skipBlockComment "/*" "*/")
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme sc
+
+symbol :: String -> Parser String
+symbol = L.symbol sc
+
+startIdentChar :: Parser Char
+startIdentChar = letterChar <|> char '_' <|> char '$'
+
+identChar :: Parser Char
+identChar = alphaNumChar <|> char '_' <|> char '$'
+
+identifier :: Parser String
+identifier = lexeme ((:) <$> startIdentChar <*> many identChar)
+
+integer :: Parser Integer
+integer = lexeme L.decimal
+
+parens :: Parser a -> Parser a
+parens = between (symbol "(") (symbol ")")
+
+commaSep :: Parser a -> Parser [a]
+commaSep p = p `sepBy` symbol ","
+
+pKeyword :: String -> Parser String
+pKeyword w = lexeme (string w <* notFollowedBy identChar)
+
+pPrimaryType :: Parser Type
+pPrimaryType = choice
+    [ TInt <$ pKeyword "int"
+    , TBool <$ pKeyword "bool"
+    , TUnit <$ pKeyword "unit"
+    , parens coreType
+    ]
+
+coreType :: Parser Type
+coreType = makeExprParser pPrimaryType coreTypeTable
+
+coreTypeTable :: [[Operator Parser Type]]
+coreTypeTable = [[InfixR (TPair <$ symbol "*")]
+                ,[InfixR (TSum <$ symbol "+")]]
+
+pPrimaryExpr :: Parser Expr
+pPrimaryExpr = choice
+    [ EInt . fromInteger <$> integer
+    , EBool True <$ pKeyword "true"
+    , EBool False <$ pKeyword "false"
+    , pTuple
+    , try (ECall <$> identifier <*> parens (commaSep coreExpr))
+    , EVar <$> (identifier  <* notFollowedBy (symbol "("))
+    ]
+
+pTuple :: Parser Expr
+pTuple = go <$> parens (commaSep coreExpr) where
+    go [] = EUnit
+    go [e] = e
+    go [e1, e2] = EPair e1 e2
+    go (e:es) = EPair e (go es)
+
+
+coreExpr :: Parser Expr
+coreExpr = choice 
+    [ pKeyword "inl" *> (EInl <$> pPrimaryExpr)
+    , pKeyword "inr" *> (EInr <$> pPrimaryExpr)
+    , pKeyword "fst" *> (EFst <$> pPrimaryExpr)
+    , pKeyword "snd" *> (ESnd <$> pPrimaryExpr)  
+    , pPrimaryExpr
+    ]
+
+coreStmt :: Parser Stmt
+coreStmt = choice
+    [ SAlloc <$> (pKeyword "let" *> identifier) <*> (symbol ":" *> coreType)
+    , SReturn <$> (pKeyword "return" *> coreExpr)
+    , SBlock <$> between (symbol "{") (symbol "}") (many coreStmt)
+    , SCase <$> (pKeyword "match" *> coreExpr <* pKeyword "with") <*> (symbol "{" *> many coreAlt <* symbol "}")
+    , SFunction <$> (pKeyword "function" *> identifier) <*> (parens (commaSep coreArg)) <*> (symbol "->" *> coreType) 
+                <*> (symbol "{" *> many coreStmt <* symbol "}")
+    , try (SAssign <$> (coreExpr <* symbol ":=") <*> coreExpr)
+    , SExpr <$> coreExpr
+    ]
+
+coreArg :: Parser Arg
+coreArg = TArg <$> identifier <*> (symbol ":" *> coreType)
+
+coreAlt :: Parser Alt  -- FIXME: distinguish inl/inr
+coreAlt = choice
+    [ Alt <$> (pKeyword "inl" *> identifier <* symbol "=>") <*> coreStmt
+    , Alt <$> (pKeyword "inr" *> identifier <* symbol "=>") <*> coreStmt
+    ]
+
+coreProgram :: Parser Core
+coreProgram = sc *> (Core <$> many coreStmt) <* eof
