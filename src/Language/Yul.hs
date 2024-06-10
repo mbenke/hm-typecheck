350dba62833d48ae4bb3b80afdb663032772f248
Marcin Benke <marcin@benke.org>
2024-06-10 12:00:55 +0200

    Language.Yul

---
+++ src/Language/Yul.hs
@@ -0,0 +1,127 @@
+module Language.Yul where
+
+import Common.Pretty
+
+newtype Yul = Yul { yulStmts :: [YulStatement] }
+instance Show Yul where show = render . pretty
+instance Show YulStatement where show = render . pretty
+instance Show YulExpression where show = render . pretty
+instance Show YulLiteral where show = render . pretty
+
+type Name = String
+type YArg = Name
+type YReturns = Maybe [Name]
+pattern YNoReturn :: Maybe a
+pattern YNoReturn = Nothing
+pattern YReturns :: a -> Maybe a
+pattern YReturns a = Just a
+pattern YulAlloc :: Name -> YulStatement
+pattern YulAlloc name = YulLet [name] Nothing
+
+data YulStatement
+  = YulBlock [YulStatement]
+  | YulFun String [YArg] YReturns [YulStatement] 
+  | YulLet [String] (Maybe YulExpression)
+  | YulAssign [String] YulExpression
+  | YulIf YulExpression [YulStatement]
+  | YulSwitch YulExpression [(YulLiteral, [YulStatement])] (Maybe [YulStatement])
+  | YulForLoop [YulStatement] YulExpression [YulStatement] [YulStatement]
+  | YulBreak
+  | YulContinue
+  | YulLeave
+  | YulComment String
+
+data YulExpression
+  = YulCall String [YulExpression]
+  | YulIdentifier String
+  | YulLiteral YulLiteral
+
+data YulLiteral
+  = YulNumber Integer
+  | YulString String
+  | YulTrue
+  | YulFalse
+
+yulInt :: Integral i => i -> YulExpression
+yulInt = YulLiteral . YulNumber . fromIntegral
+
+yulBool :: Bool -> YulExpression
+yulBool True = YulLiteral YulTrue
+yulBool False = YulLiteral YulFalse
+
+instance Pretty Yul where
+  pretty (Yul stmts) = vcat (map pretty stmts)
+
+instance Pretty YulStatement where
+  pretty (YulBlock stmts) =
+    lbrace
+      $$ nest 4 (vcat (map pretty stmts))
+      $$ rbrace
+  pretty (YulFun name args rets stmts) =
+    text "function"
+      <+> text name
+      <+> prettyargs
+      <+> prettyrets rets
+      <+> lbrace
+      $$ nest 4 (vcat (map pretty stmts))
+      $$ rbrace
+    where
+        prettyargs = parens (hsep (punctuate comma (map text args)))
+        prettyrets Nothing = empty
+        prettyrets (Just rs) = text "->" <+> (hsep (punctuate comma (map text rs)))
+  pretty (YulLet vars expr) = 
+    text "let" <+> hsep (punctuate comma (map text vars)) 
+               <+> maybe empty (\e -> text ":=" <+> pretty e) expr 
+  pretty (YulAssign vars expr) = hsep (punctuate comma (map text vars)) <+> text ":=" <+> pretty expr
+  pretty (YulIf cond stmts) = text "if" <+> parens (pretty cond) <+> pretty (YulBlock stmts)
+  pretty (YulSwitch expr cases def) =
+    text "switch"
+      <+> (pretty expr)
+      $$ nest 4 (vcat (map (\(lit, stmts) -> text "case" <+> pretty lit <+> pretty (YulBlock stmts)) cases))
+      $$ maybe empty (\stmts -> text "default" <+> pretty (YulBlock stmts)) def
+  pretty (YulForLoop pre cond post stmts) = 
+    text "for" <+> braces (hsep  (map pretty pre))
+               <+> pretty cond  
+               <+> hsep (map pretty post) <+> pretty (YulBlock stmts)
+  pretty YulBreak = text "break"
+  pretty YulContinue = text "continue"
+  pretty YulLeave = text "leave"
+  pretty (YulComment c) = text "/*" <+> text c <+> text "*/"
+
+instance Pretty YulExpression where
+  pretty (YulCall name args) = text name >< parens (hsep (punctuate comma (map pretty args)))
+  pretty (YulIdentifier name) = text name
+  pretty (YulLiteral lit) = pretty lit
+
+instance Pretty YulLiteral where
+  pretty (YulNumber n) = integer n
+  pretty (YulString s) = doubleQuotes (text s)
+  pretty YulTrue = text "true"
+  pretty YulFalse = text "false"
+
+{- | wrap a Yul chunk in a Solidity function with the given name 
+   assumes result is in a variable named "_result"
+-}
+wrapInSolFunction :: Pretty a => Name -> a -> Doc
+wrapInSolFunction name yul = text "function" <+> text name <+> prettyargs <+> text " public pure returns (uint256 _mainresult)" <+> lbrace
+  $$ nest 2 assembly
+  $$ rbrace
+  where
+    assembly = text "assembly" <+> lbrace
+      $$ nest 2 (pretty yul)
+      $$ rbrace
+    prettyargs = parens empty
+
+wrapInContract :: Name -> Name -> Doc -> Doc
+wrapInContract name entry body = empty
+  $$ text "pragma solidity ^0.8.23;"
+  $$ text "import {console,Script} from \"lib/stdlib.sol\";"
+  $$ text "contract" <+> text name <+> text "is Script"<+> lbrace
+  $$ nest 2 run 
+  $$ nest 2 body
+  $$ rbrace
+  
+  where
+    run = text "function run() public view" <+> lbrace
+      $$ nest 2 (text "console.log(\"RESULT --> \","<+> text entry >< text ");")
+      $$ rbrace $$ text ""
---
+++ src/Language/Yul/Parser.hs
@@ -0,0 +1,95 @@
+module Language.Yul.Parser(parseYul) where
+{-
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Data.Void
+-}
+import Common.LightYear
+import Text.Megaparsec.Char.Lexer qualified as L
+import Language.Yul
+
+
+parseYul :: String -> Yul
+parseYul = runMyParser yulProgram
+
+sc :: Parser ()
+sc = L.space space1
+             (L.skipLineComment "//")
+             (L.skipBlockComment "/*" "*/")
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme sc
+
+symbol :: String -> Parser String
+symbol = L.symbol sc
+
+startIdentChar :: Parser Char
+startIdentChar = letterChar <|> char '_' <|> char '$'
+
+identChar :: Parser Char
+identChar = alphaNumChar <|> char '_' <|> char '$'
+
+identifier :: Parser String
+identifier = lexeme ((:) <$> startIdentChar <*> many identChar)
+
+integer :: Parser Integer
+integer = lexeme L.decimal
+
+parens :: Parser a -> Parser a
+parens = between (symbol "(") (symbol ")")
+
+commaSep :: Parser a -> Parser [a]
+commaSep p = p `sepBy` symbol ","
+
+pKeyword :: String -> Parser String
+pKeyword w = lexeme (string w <* notFollowedBy identChar)
+
+yulExpression :: Parser YulExpression
+yulExpression = choice
+    [ YulLiteral <$> yulLiteral
+    , YulIdentifier <$> identifier
+    , YulCall <$> identifier <*> parens (commaSep yulExpression)
+    ]
+
+yulLiteral :: Parser YulLiteral
+yulLiteral = choice
+    [ YulNumber <$> integer
+    , YulString <$> (char '"' *> manyTill L.charLiteral (char '"'))
+    , YulTrue <$ pKeyword "true"
+    , YulFalse <$ pKeyword "false"
+    ]
+
+yulStatement :: Parser YulStatement
+yulStatement = choice
+    [ YulBlock <$> yulBlock
+    , yulFun
+    , YulLet <$> (pKeyword "let" *> commaSep identifier) <*> optional (symbol ":=" *> yulExpression)
+    , YulIf <$> (pKeyword "if" *> yulExpression) <*> yulBlock
+    , YulSwitch <$>
+        (pKeyword "switch" *> yulExpression) <*>
+        many yulCase <*>
+        optional (pKeyword "default" *> yulBlock)
+    , YulAssign <$> commaSep identifier <*> (symbol ":=" *> yulExpression)
+    ]
+
+yulBlock :: Parser [YulStatement]
+yulBlock = between (symbol "{") (symbol "}") (many yulStatement)
+
+yulCase :: Parser (YulLiteral, [YulStatement])
+yulCase = do
+    _ <- pKeyword "case"
+    lit <- yulLiteral
+    stmts <- yulBlock
+    return (lit, stmts)
+
+yulFun :: Parser YulStatement
+yulFun = do
+    _ <- symbol "function"
+    name <- identifier
+    args <- parens (commaSep identifier)
+    rets <- optional (symbol "->" *> (commaSep identifier))
+    stmts <- yulBlock
+    return (YulFun name args rets stmts)
+
+yulProgram :: Parser Yul
+yulProgram = sc *> (Yul <$> many yulStatement) <* eof
